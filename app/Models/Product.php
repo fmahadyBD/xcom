6e1329e63535505453d630b01e53e6e96bf95274
Mahady Hasan Fahim <fmahady01@gmail.com>
2024-01-25 16:38:39 +0100

    Relation established beetoween to table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    //Eloquent Relationships
     use HasFactory;
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
